129142efda3e90b7439fc14a6dd9cd39f1a2e1cf
Moureen <moureenham@gmail.com>
2021-10-22 22:11:59 +0530

    message display functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 import "./App.css";
 import { connect } from "react-redux";
+
 import ColorPanel from "./ColorPanel/ColorPanel";
 import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 
-const App = ({ currentUser }) => (
+const App = ({ currentUser, currentChannel }) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
-    <SidePanel currentUser={currentUser} />
+    <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+      />
     </Grid.Column>
 
     <Grid.Column width={4}>
@@ -24,5 +29,7 @@ const App = ({ currentUser }) => (
 
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel,
 });
+
 export default connect(mapStateToProps)(App);
